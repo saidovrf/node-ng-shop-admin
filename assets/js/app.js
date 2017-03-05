92bc15189be677d8a2e0ef50cfabbef126578885
Rustam S <saidov.rf@gmail.com>
2017-03-05 21:45:38 +0300

    New API support
    - created new post method for HTTP factory;
    - added support for creating new product;
    - added support for creating new category;
    - some refactoring in code;

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,44 +10,166 @@
 				  method: 'GET',
 				  url: url
 				}).then(function successCallback(response) {
-					if (response.status === 200) {
+					if (response.status === 200) {	
 						callback(response.data);
 					} else {
-						// error aler
+						// error alert
 					}
-					
 				}, function errorCallback(response) {
-					// error aler
+					// error alert
+				});
+
+			},
+			post: function(url, data, callback) {
+				$http({
+				  method: 'POST',
+				  url: url,
+				  data: data
+				}).then(function successCallback(response) {
+					if (response.status === 201) {
+						callback(response.data)
+					} else {
+						// alert
+					}
+				}, function errorCallback(response) {
+					if (response.status === 400 && response.data.code === "E_VALIDATION") {	
+						alert('Заполните все обязательные поля')
+					} else {
+						// error alert
+					}
 				});
 
 			}
 		};
 	}])
 
+	// .factory('Products', ['$rootScope', function($rootScope){
+	// 	var _productsList = [];
+	// 	return {
+	// 		get: function() {
+	// 			return _productsList;
+	// 		},
+	// 		set: function(value) {
+	// 			_productsList = value;
+	// 			$rootScope.$broadcast('products_changed', value);
+	// 		}
+	// 	};
+	// }])
+
+	// .factory('Categories', ['$rootScope', function($rootScope){
+	// 	var _categoriesList = [];
+	// 	return {
+	// 		get: function() {
+	// 			return _categoriesList;
+	// 		},
+	// 		set: function(value) {
+	// 			_categoriesList = value;
+	// 			$rootScope.$broadcast('categories_changed', value);
+	// 		}
+	// 	};
+	// }])
+
 	.controller('CommonCtrl', ['$scope', 'HTTP', CommonCtrl]);
 
 	function CommonCtrl($scope, HTTP){
 		var vm = this;
 
-		vm.defaultCategoryCount
 		vm.products = [];
 		vm.categories = [];
 		vm.categoriesCount = [];
-		
+
+		vm.product = {
+			name: '',
+			price_main: 0,
+			price: 0,
+			category: '0'
+		};
+
+		vm.category = {
+			name: ''
+		};
+
+		vm.newProduct = newProduct;
+		vm.newCategory = newCategory;
+
+
+		function newProduct() {
+			if (vm.product.name.split(' ').join('').length === 0) {
+				alert('Введите название'); return;
+			}
+			if (vm.product.price_main <= 0 || vm.product.price <= 0) {
+				alert('Значение любой стоимости должно быть выше 0'); return;
+			}
+
+			$('#createProduct').modal('toggle');
+
+			if (vm.product.category === '0') {
+				delete vm.product.category;
+			}
+
+			HTTP.post('/products', vm.product, function(response) {
+				vm.products.push(response);
+
+				if (response.category) {
+					vm.categoriesCount[response.category]++;
+				} else {
+					vm.categoriesCount["0"]++;
+				}
+
+				vm.product = {
+					name: '',
+					price_main: 0,
+					price: 0,
+					category: '0'
+				};
+			})
+		}
+
+		function newCategory() {
+
+			if (vm.category.name.split(' ').join('').length === 0) {
+				alert('Введите название'); return;
+			}
+
+			$('#createCategory').modal('toggle');
+
+			HTTP.post('/categories', vm.category, function(response) {
+				vm.categories.push(response);
+
+				vm.categoriesCount[response.id] = 0;
+
+				vm.category = {
+					name: ''
+				};
+			})
+		}
+
+		// $scope.$on('products_changed', function(e, d) {
+		// 	vm.products = d;
+		// })
+		// $scope.$on('categories_changed', function(e, d) {
+		// 	vm.categories = d;
+		// })
+
 		HTTP.get('/categories', function(response) {
-			vm.categories = response;
+			vm.categories = [{
+				name: 'Без названия',
+				id: '0'
+			}];
+			vm.categories = vm.categories.concat(response);
+
+			for (var i = 0; i < vm.categories.length; i++) {
+				vm.categoriesCount[vm.categories[i].id] = 0;
+			}
 
 			HTTP.get('/products', function(response) {
 				vm.products = response;
+				
 				vm.categoriesCount["0"] = 0;
 
 				for (var i = 0; i < vm.products.length; i++) {
 					if (vm.products[i].category) {
-						if (vm.categoriesCount[vm.products[i].category.id]) {
-							vm.categoriesCount[vm.products[i].category.id]++;
-						} else {
-							vm.categoriesCount[vm.products[i].category.id] = 1;
-						}
+						vm.categoriesCount[vm.products[i].category.id]++;
 					} else {
 						vm.categoriesCount["0"]++;
 					}
