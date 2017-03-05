e76814ed630fb90972aee6792db0c01a24420cf6
Rustam S <saidov.rf@gmail.com>
2017-03-06 02:45:41 +0300

    Design and refactoring
    - added background image;
    - added some nice black-white design style for a page;
    - changed some file structures;
    - some code refactoring.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -34,7 +34,9 @@ var jsFilesToInject = [
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js'
+  'js/**/*.js',
+
+  'js/modules/**/*.js',
 ];
 
 
